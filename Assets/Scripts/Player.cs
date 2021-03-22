d275968f40fa7807cc7fae84642e2262d6776191
LAPTOP-AMTQI36U\aadit <aadit3003@gmail.com>
2021-03-22 21:35:36 +0530

    Added buttons and Starting Screen, Success level

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -67,7 +67,6 @@ public class Player : MonoBehaviour
 
         if(CrossPlatformInputManager.GetButtonDown("Vertical"))
         {
-            myAnimator.SetTrigger("Door In");
             FindObjectOfType<Door>().StartLoadingNextLevel();
 
         }
