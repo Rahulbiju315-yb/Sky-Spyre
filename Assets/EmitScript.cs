5a77bd596c0a7cd813d42e6f687ca013b075b45e
Rahulbiju315 <rahulpikslit@gmail.com>
2021-04-11 18:45:42 +0530

    Emit Light Particles On Attack

--- Assets/EmitScript.cs
+++ Assets/EmitScript.cs
@@ -5,20 +5,40 @@ using UnityEngine;
 public class EmitScript : MonoBehaviour
 {
     private Animator playerAnimator;
+    private GameObject player;
+
+    private float lastTime;
+    private bool isAlive;
+
+    public GameObject litPSprefab;
+    public float emissionVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        playerAnimator = GameObject.Find("Player").GetComponent<Animator>();
+        player = GameObject.Find("Player");
+        playerAnimator = player.GetComponent<Animator>();
+        lastTime = 0;
+        isAlive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        ParticleSystem particles = gameObject.GetComponent<ParticleSystem>();
-        Debug.Log("sdfds" + playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attacking"));
-        if (!particles.isEmitting && playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attacking"))
+        if (!isAlive && playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attacking"))
         {
-            particles.Play();
+            Debug.Log("is alive" + isAlive);
+            lastTime = Time.time;
+            litPSprefab = (GameObject)Instantiate(litPSprefab, player.transform.position, transform.rotation);
+
+            Vector2 to = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 vel = (to - new Vector2(player.transform.position.x, player.transform.position.y)).normalized * emissionVelocity;
+            litPSprefab.GetComponent<Rigidbody2D>().velocity = new Vector2(emissionVelocity, 0);
+            isAlive = true;
+        }
+
+        if(isAlive && Time.time - lastTime > 1)
+        {
+            isAlive = false;
         }
     }
 }
