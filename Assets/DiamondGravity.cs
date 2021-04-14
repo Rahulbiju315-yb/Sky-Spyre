b90d825f3c0c72800622d6e4568982d7e89284fb
Asish-Juttu <f20190039@pilani.bits-pilani.ac.in>
2021-04-14 11:16:59 +0530

    Made a separate script for gravity Diamond.

--- Assets/DiamondGravity.cs
+++ Assets/DiamondGravity.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickupDiamond : MonoBehaviour
+public class DiamondGravity : MonoBehaviour
 {
     [SerializeField] AudioClip diamondPickupSFX;
+    public Rigidbody2D rb;    
+    public Transform tr;    
+    public Vector3 set;
+    public Gravity script;
 
     [SerializeField] int diamondPoints = 100;
     private void OnTriggerEnter2D(Collider2D collision)
@@ -12,7 +16,8 @@ public class PickupDiamond : MonoBehaviour
         AudioSource.PlayClipAtPoint(diamondPickupSFX, Camera.main.transform.position); // The camera is following the player, so this works
 
         FindObjectOfType<GameSession>().AddScore(diamondPoints); // increase the score whenever player collects a diamond
-
+        script.g = !script.g;
         Destroy(gameObject); // Make the heart disappear, when player touches them
+        
     }
 }
