ed67c6851ada6bd86aedf62686ba7ab886385496
Asish-Juttu <f20190039@pilani.bits-pilani.ac.in>
2021-04-16 19:43:06 +0530

    Stuff

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -23,7 +23,7 @@ public class Gravity : MonoBehaviour
     void Update(){
         if(g){
             this.GetComponent<Rigidbody2D>().AddForce(f);
-            player.jumpSpeed = +75;
+            player.jumpSpeed = +150;
             tr.rotation=Quaternion.Euler(0,0,0);
         }
         else {
