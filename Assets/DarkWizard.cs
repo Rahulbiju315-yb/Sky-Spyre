5ef36fbc96edb9488b9dc4230ddc9d05ef6ae583
Asish-Juttu <f20190039@pilani.bits-pilani.ac.in>
2021-04-17 12:54:55 +0530

    Update DarkWizard.cs

--- Assets/DarkWizard.cs
+++ Assets/DarkWizard.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class DarkWizard : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Animator animator;
+    int hit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f); // Check the sign of the velocity
+        hit=0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(hit==5){
+            StartCoroutine(OnDeath());
+        }
+    }
+
+    IEnumerator OnDeath(){
+        gameObject.GetComponent<ProjectileSource>().Stop();
+        animator.speed *= 0.25f;
+        animator.SetTrigger("Death");
+        yield return new WaitForSeconds(4f);
+        Destroy(GameObject.Find("Boss"));
     }
 }
+
+
